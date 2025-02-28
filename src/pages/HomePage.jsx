fe1782fe7bd7c940f518fd7b8b3ff7695978daee
Anuj Pandey <anujpandey1701@yahoo.com>
2025-02-28 21:17:18 +0530

    init

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../components/navbar/NavBar';
+
+const HomePage = () => {
+    return (
+        <>
+            <NavBar/>
+        </>
+    );
+}
+
+
+export default HomePage;
